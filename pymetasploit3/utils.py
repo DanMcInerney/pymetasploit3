422753b93d04095414d6beae395c96c838b9ae6e
Matthew C. Jones, CPA, CISA, OSCP, CCFE <mjones@isaudits.com>
2021-08-04 23:25:37 -0400

    fix default port in help text

--- pymetasploit3/utils.py
+++ pymetasploit3/utils.py
@@ -17,7 +17,7 @@ def parseargs():
     p.add_option("-S", dest="ssl", help="Disable SSL on the RPC socket", action="store_false", default=True)
     p.add_option("-U", dest="username", help="Specify the username to access msfrpcd", metavar="opt", default="msf")
     p.add_option("-a", dest="server", help="Connect to this IP address", metavar="host", default="127.0.0.1")
-    p.add_option("-p", dest="port", help="Connect to the specified port instead of 55552", metavar="opt", default=55553)
+    p.add_option("-p", dest="port", help="Connect to the specified port instead of 55553", metavar="opt", default=55553)
     o, a = p.parse_args()
     if o.password is None:
         print('[-] Error: a password must be specified (-P)\n')
