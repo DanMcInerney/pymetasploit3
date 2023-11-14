5ac8bfa6c3798b0243d4a17b582177789412092f
Blake <80996688+BlakeTnr@users.noreply.github.com>
2023-11-13 22:36:02 -0500

    test

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 setup(
     name='testpymetasploit3',
     author='Dan McInerney',
-    version='1.4VERSION_MINOR_RELEASE',
+    version='1.5.GITHUB_RUN_NUMBER.GITHUB_RUN_ATTEMPT',
     author_email='danhmcinerney@gmail.com',
     description='A full-fledged msfrpc library for Metasploit framework.',
     license='GPL',
