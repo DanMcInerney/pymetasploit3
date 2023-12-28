952237cbf988131f01851e912792b610a2a34436
Dan McInerney <dan.h.mcinerney@gmail.com>
2023-12-28 14:06:16 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 setup(
     name='pymetasploit3',
     author='Dan McInerney',
-    version='1.0.GITHUB_RUN_NUMBER.GITHUB_RUN_ATTEMPT',
+    version='1.0.5',
     author_email='danhmcinerney@gmail.com',
     description='A full-fledged msfrpc library for Metasploit framework.',
     license='GPL',
